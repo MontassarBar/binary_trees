7eca0a3699a97826a2171119046771a6d9bd2958
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-22 09:31:40 +0100

    Insert left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+
+binary_tree_t *node = malloc(sizeof(binary_tree_t));
+if (node == NULL)
+	return (NULL);
+if (parent == NULL)
+	return (NULL);
+node->parent = parent;
+node->n = value;
+node->left = NULL;
+node->right = NULL;
+if (parent->left != NULL)
+{
+	node->left = parent->left;
+	parent->left->parent = node;
+	parent->left = node;
+}
+else
+	parent->left = node;
+return (node);
+}
