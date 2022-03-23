a43061e6bacfd3c3ef4c6ca38a11ecc6626d3a99
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-23 16:50:09 +0100

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,9 +8,11 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
+
 if (parent == NULL)
 	return (NULL);
-node = binary_tree_nodes(parent, value);
+
+node = binary_tree_node(parent, value);
 if (node == NULL)
 	return (NULL);
 if (parent->left != NULL)
