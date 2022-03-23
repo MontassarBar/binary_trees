e6f51954f0060a4264232605a10d132dde839157
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-23 16:52:07 +0100

    Insert right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,22 +7,18 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-binary_tree_t *node = malloc(sizeof(binary_tree_t));
-if (node == NULL)
-	return (NULL);
+binary_tree_t *node;
 if (parent == NULL)
 	return (NULL);
-node->parent = parent;
-node->n = value;
-node->left = NULL;
-node->right = NULL;
+node = binary_tree_node(parent, value);
+if (node == NULL)
+	return (NULL);
 if (parent->right != NULL)
 {
 	node->right = parent->right;
 	parent->right->parent = node;
 	parent->right = node;
 }
-else
-	parent->right = node;
+parent->right = node;
 return (node);
 }
