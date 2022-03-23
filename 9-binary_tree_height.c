38b6c361c5efb858c59adce2e0ff402359a11420
MontassarBar <montassarbarbouchi7@gmail.com>
2022-03-23 17:44:56 +0100

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,11 +6,11 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t x;
-	size_t y;
+size_t x;
+size_t y;
 if (tree == NULL)
 	return (0);
-	x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (x > y ? x : y);
+x = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+y = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+return (x > y ? x : y);
 }
